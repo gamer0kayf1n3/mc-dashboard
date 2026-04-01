46f836b173927ca0a2d105bd8214cc4cc3a33cd6
gamer0kayf1n3 <muser0943@gmail.com>
2026-04-01 12:48:09 +0800

    functional playerlist route api

--- backend/routes/players.py
+++ backend/routes/players.py
@@ -1,7 +1,21 @@
-from flask import Blueprint
+import os
+
+from flask import Blueprint, jsonify, request
+import subprocess
+import mcrcon
+import configload
+
+config = configload.Config()
 
 players_bp = Blueprint('players', __name__, url_prefix='/api')
 
 @players_bp.route('/players', methods=['GET'])
 def get_players():
-    return {"message": "List of players"}
+    players = []
+    #try:
+    with mcrcon.MCRcon("localhost", config.RCON_PASSWORD, config.RCON_PORT) as mcr:
+        response = mcr.command("list")
+        players = response.strip().split(": ")[-1].split(", ") if ": " in response else []
+    return jsonify({'players': players}), 200
+    #except Exception as e:
+    #    return jsonify({'error': str(e)}), 500
